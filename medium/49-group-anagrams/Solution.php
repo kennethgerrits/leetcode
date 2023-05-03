0590ab138163590e48822d29b9a3095a73d34df5
Kenneth Gerrits <kennethgerrits@gmail.com>
2023-05-03 14:47:54 +0200

    fix autogen namespace

--- medium/49-group-anagrams/Solution.php
+++ medium/49-group-anagrams/Solution.php
@@ -39,7 +39,7 @@ final class Solution
     }
 }
 
-new wealth\Solution();
+new Solution();
 
 /**
  * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
